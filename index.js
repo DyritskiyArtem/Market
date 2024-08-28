8980441ac9b5da8fcad405839549bee99d858188
DyritskiyArtem <dyritskiyartem2012@gmail.com>
2024-08-28 17:42:20 +0300

    add style and register

--- index.js
+++ index.js
@@ -1,58 +1,100 @@
 const express = require('express');
+const pug = require('pug');
+const fs = require('fs');
+
+const compiledFunction = pug.compileFile("userList.pug");
+const compiledFunctionRegister = pug.compileFile("register.pug");
+const compiledFunctionLogin = pug.compileFile("login.pug");
 
 const app = express();
 const port = 1000;
 
-let users = [];
+let usersList = require("./usersList.json");
+const { password } = require('telegram');
 
 app.use(express.urlencoded({ extended: true })); 
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
+app.get('/register', (req, res) => {
+    let {error} = req.query
 
-    let html = `
-    <html>
-    <head>
-        <title>Market</title>
-
-        <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
-        <link rel="manifest" href="favicons/site.webmanifest">
-        <link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#faebd6">
-        <meta name="msapplication-TileColor" content="#faebd6">
-        <meta name="theme-color" content="#faebd6">
-
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    </head>
-    <body>
-        <form action="/" method="POST">
-            <input type="text" name="name" placeholder="Ім'я" required />
-            <input type="text" name="login" placeholder="Логін" required />
-            <input type="password" name="password" placeholder="Пароль" required />
-            <button type="submit">Зареєструватися</button>
-        </form>
-
-    </body>
-    </html>
-    `
-  res.send(html);
+    res.send(compiledFunctionRegister({
+        error
+    }));
 });
 
-app.post('/', (req, res) => {
+app.get('/login', (req, res) => {
+    let {error} = req.query
+
+    res.send(compiledFunctionLogin({
+        error
+    }));
+});
+
+app.post('/register', (req, res) => {
     const { name, login, password } = req.body;
-    users.push({ name, login });
 
-    let userList = '<h2>Список користувачів:</h2><ul>';
-
-    userList += "<ul>";
-    for (let i = 0; i < users.length; i++) {
-        userList += `<li>Ім'я: ${users[i].name}, Логін: ${users[i].login}</li>`;
+    if (name.length < 3 || name.length > 25) {
+        res.redirect('/register?error=Name must be between 3 and 25 characters')
+        return;
     }
-    userList += "</ul>";
 
-  res.send(userList);
+    if (password.length < 8 || password.length > 25) {
+        res.redirect('/register?error=Password must be between 8 and 25 characters')
+        return;
+    }
+
+    for (let i = 0; i < usersList.length; i++) {
+        const user = usersList[i];
+        if (user.login === login) {
+            res.redirect('/register?error=This login already exists');
+            return;
+        }
+    }
+
+    if (login.length < 3 || login.length > 25) {
+        res.redirect('/register?error=Login must be between 3 and 25 characters')
+        return;
+    }
+
+    usersList.push({ name, login, password });
+    let json = JSON.stringify(usersList);
+    fs.writeFileSync("./usersList.json", json);
+
+    res.send(compiledFunction({
+        login: login,
+        users: usersList,
+    }));
+
+});
+
+app.post('/login', (req, res) => {
+    const {login, password} = req.body;
+
+    let userFound = false;
+    for (let i = 0; i < usersList.length; i++) {
+        const user = usersList[i];
+        if (user.login === login) {
+            userFound = true;
+
+            if (user.password !== password) {
+                res.redirect('/login?error=Your login or password is incorrect');
+                return;
+            }
+        }
+    }
+
+    if (userFound == false) {
+        res.redirect('/login?error=Your login or password is incorrect');
+        return;
+    }
+
+    res.send(compiledFunction({
+        login: login,
+        users: usersList,
+    }));
 });
 
 app.listen(port, () => {
-  console.log(`Сервер запущено на http://localhost:${port}`);
+  console.log(`The server is running on http://localhost:${port}`);
 });
