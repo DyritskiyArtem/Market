158b1ad42f87d1ac54ebfea4bab686ad2c7ed5ab
DyritskiyArtem <dyritskiyartem2012@gmail.com>
2024-08-30 13:17:29 +0300

    add form addItem

--- index.js
+++ index.js
@@ -5,15 +5,42 @@ const fs = require('fs');
 const compiledFunction = pug.compileFile("userList.pug");
 const compiledFunctionRegister = pug.compileFile("register.pug");
 const compiledFunctionLogin = pug.compileFile("login.pug");
+const compiledFunctionMyAds = pug.compileFile("myAds.pug");
+const compiledFunctionAddItem= pug.compileFile("addItem.pug");
 
 const app = express();
 const port = 1000;
 
 let usersList = require("./usersList.json");
-const { password } = require('telegram');
 
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.static("public"));
+
+app.get('/myAds', (req, res) => {
+    res.send(compiledFunctionMyAds({}));
+});
+
+app.get('/addItem', (req, res) => {
+    let {error} = req.query
+
+    res.send(compiledFunctionAddItem({
+        error
+    }));
+});
+
+app.post('/addItem', (req, res) => {
+    const {description, price} = req.body;
+
+    if (description.length < 10 || description.length > 150) {
+        res.redirect('/addItem?error=Description must be between 10 and 150 characters');
+        return;
+    }
+
+    if (price.length < 0 || price.length > 6) {
+        res.redirect('/addItem?error=The price of the product must be from 0 to 100,000$');
+        return;
+    }
+});
 
 app.get('/register', (req, res) => {
     let {error} = req.query
@@ -35,12 +62,18 @@ app.post('/register', (req, res) => {
     const { name, login, password } = req.body;
 
     if (name.length < 3 || name.length > 25) {
-        res.redirect('/register?error=Name must be between 3 and 25 characters')
+        res.redirect('/register?error=Name must be between 3 and 25 characters');
         return;
     }
 
     if (password.length < 8 || password.length > 25) {
-        res.redirect('/register?error=Password must be between 8 and 25 characters')
+        res.redirect('/register?error=Password must be between 8 and 25 characters');
+        return;
+    }
+
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,25}$/;
+    if (!passwordRegex.test(password)) {
+        res.redirect('/register?error=Password must contain at least one uppercase letter, one lowercase letter, and one digit')
         return;
     }
 
---
+++ public/burger.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const burger = document.querySelector('.burger');
+    const navMenu = document.querySelector('.nav-menu');
+    
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    });
+});
